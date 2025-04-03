fa4a80a7366b591aae4eb263ec82a44b493c0a63
Ambi <ptlc.ptlc.ptlc@gmail.com>
2025-04-03 20:43:24 +0200

    Transformation en paquet NPM + README + quelques fix

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Rebus
+# Rebus-fr
 
-Petit fonction JavaScript pour créer des rébus avec des emojis à partir de phrase en français.
+Petite fonction JavaScript pour créer des rébus avec des emojis à partir de phrase en français.
+
+
+## Exemple
+
+`Mon chat a mangé un poisson` devient `Ⓜ️on 🐱 🅰️ Ⓜ️👼é 1️⃣ 🪴a🔊`
+
+`L'imprimeur de paris vend son âme au démon` devient `l'1️⃣🅿🍚Ⓜ️🥚r 2️⃣ 👣🍚s 🍃d 🔊 âⓂ️e 💧 🎲Ⓜ️on`
+
+`Antoine se rabote dans son lit joyeusement` devient `📅🫵🪢 se 🐀👢 🦷 🔊 🛏️ 😀🥚seⓂ️📅t`
+
+
+## Utilisation
+
+```js
+const { createRebus } = require("rebus-fr");
+
+var rebus = createRebus("Voici un rébus");
+
+console.log(rebus);
+```
--- test.js
+++ test.js
@@ -1,7 +1,14 @@
-var readline = require("readline").createInterface(process.stdin, process.stdout);
+const { createRebus } = require("./rebus.js");
 
-var Rebus = require("./rebus.js");
+function checkEquals(a, b) {
+    if (a == b) {
+        console.log(`✔️ ${a}`);
+    } else {
+        console.log(`✖️ "${a}" != "${b}"`);
+        process.exit(1);
+    }
+}
 
-readline.on("line", line => {
-    console.log(Rebus.createRebus(line));
-});
+checkEquals(createRebus("Mon chat a mangé un poisson"), "Ⓜ️on 🐱 🅰️ Ⓜ️👼é 1️⃣ 🪴🅰️🔊");
+checkEquals(createRebus("L'imprimeur de paris vend son âme au démon"), "l'1️⃣🅿🍚Ⓜ️🥚r 2️⃣ 👣🍚s 🍃d 🔊 âⓂ️e 💧 🎲Ⓜ️on");
+checkEquals(createRebus("Antoine se rabote dans son lit joyeusement"), "📅🫵🪢 se 🐀👢 🦷 🔊 🛏️ 😀🥚seⓂ️📅t");
